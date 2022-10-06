60b1fd04782fa12d47ccb18dfcb99cdd74fb7fbf
Frank <franciscotame0@gmail.com>
2022-10-05 19:39:27 -0500

    Ask for location permissions

---
+++ app/src/main/java/mx/tec/bamxmorelos/LocationTest.kt
@@ -0,0 +1,14 @@
+package mx.tec.bamxmorelos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.location.Location
+import android.location.LocationManager
+
+class LocationTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_test)
+
+    }
+}
--- app/src/main/java/mx/tec/bamxmorelos/MainActivity.kt
+++ app/src/main/java/mx/tec/bamxmorelos/MainActivity.kt
@@ -1,26 +1,50 @@
 package mx.tec.bamxmorelos
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager.BadTokenException
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+    lateinit var locationManager: LocationManager
+    lateinit var mapa: GoogleMap
     //lateinit var nombre:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-
         val clMain = findViewById<ConstraintLayout>(R.id.clMain)
-
         val sharedPreference = getSharedPreferences("archivo", Context.MODE_PRIVATE)
+
+        requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if(ActivityCompat.checkSelfPermission(this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) //Si el permiso se concedió, se activa el GPS
+
+        /*override fun onLocationChanged(location: Location) {
+            //Toast.makeText(this@MainActivity, location.latitude.toString(), Toast.LENGTH_SHORT).show()
+            var marcador: LatLng = LatLng(location.latitude, location.longitude)
+            if(inicio == true){
+                mapa.addMarker(MarkerOptions().position(marcador))
+            }
+        }*/
 
         with(sharedPreference){
             val nombre = getString("nombre" ,"#" )
@@ -33,6 +57,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         clMain.setOnClickListener(this)
+    }
+
+    val requestPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            isGranted ->
+        val message = when {
+            isGranted -> "Permiso concedido"
+            Build.VERSION.SDK_INT > Build.VERSION_CODES.M &&
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) ->
+                "Explicación"
+            else -> "Permiso denegado"
+        }
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onClick(v: View?){
